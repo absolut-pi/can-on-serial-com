6d0aa2fd509daf96bd9dff3ade32e8cfdf1985d7
illegal-instruction-co <machinetherapist2@gmail.com>
2023-09-18 13:16:38 +0300

    rem submodule

--- build.sh
+++ build.sh
@@ -1,15 +1,4 @@
 git submodule update --init --recursive
-cd vendor
-
-cd CppLinuxSerial
-mkdir build
-cd build
-cmake -DBUILD_TESTS=OFF ..
-make install
-cd ../..
-
-cd ..
-
 mkdir build
 cd build
 make install
