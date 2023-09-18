b7ac929c7b3ba3552d289d560e019ba8ab5da6e7
illegal-instruction-co <machinetherapist2@gmail.com>
2023-09-18 19:04:18 +0300

    start from beginning

--- src/CanOnSerialCom.cpp
+++ src/CanOnSerialCom.cpp
@@ -136,8 +136,8 @@ void CanOnSerialCom::ProxySerialToCan() {
             frame.can_id = stoi(splittedData[0], nullptr, 16);
             frame.can_dlc = splittedData.size() - 1;
 
-            for (int i = 0; i < frame.can_dlc; i++)
-                frame.data[i] = stoi(splittedData[i], nullptr, 16);
+            for (int i = 1; i < frame.can_dlc; i++)
+                frame.data[i - 1] = stoi(splittedData[i], nullptr, 16);
 
             write(m_canSocket, &frame, sizeof(struct can_frame));
         }
