55c18b157f60d810ec1cc05ee7c750679943b45b
illegal-instruction-co <machinetherapist2@gmail.com>
2023-09-18 18:48:02 +0300

    get last 8 byte

--- src/CanOnSerialCom.cpp
+++ src/CanOnSerialCom.cpp
@@ -80,6 +80,9 @@ void CanOnSerialCom::ProxyCanToSerial() {
             for (int i = 0; i < frame.can_dlc; i++)
                 data += fmt::format("{:02X} ", frame.data[i]);
 
+            if (data.size() > 8)
+                data = data.substr(data.size() - 8, 8);
+
             print("Data from can to serial com: {}\n", data.data());
 
             m_serialPort.Open();
