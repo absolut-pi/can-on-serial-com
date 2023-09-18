ce7be61d03559550dde1d8cc7fc6aa15017e9daf
illegal-instruction-co <machinetherapist2@gmail.com>
2023-09-18 13:44:43 +0300

    check signal received

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,21 @@
 #include "CanOnSerialCom.h"
 
+#include <iostream>
+
 #include <signal.h>
 
 CanOnSerialCom com("can0", "ttyACM0");
 
 inline void signallCallback(int signum) {
+    using namespace std;
+
+    cout << "Signal " << signum << " received!" << endl;
+
     com.ClearVirtualCan();
     exit(signum);
 }
 
 int main() {
-
     signal(SIGINT, signallCallback);
     return 0;
 }
